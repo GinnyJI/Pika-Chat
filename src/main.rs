7f9a13c7d833158e10dc3ba591ba84035c3c6733
Ginny JI <ginnychi.98@gmail.com>
2024-11-22 14:31:59 -0500

    initial aatempt

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+use yew::prelude::*;
+
+pub enum Msg {}
+
+pub struct App;
+
+impl Component for App {
+    type Message = Msg;
+    type Properties = ();
+
+    fn create(_: Self::Properties, _: ComponentLink<Self>) -> Self {
+        App
+    }
+
+    fn update(&mut self, _: Self::Message) -> ShouldRender {
+        false
+    }
+
+    fn view(&self) -> Html {
+        html! {
+            <div>
+                <h1>{ "Welcome to Yew Frontend" }</h1>
+                <p>{ "This is the starting point of your Yew application." }</p>
+            </div>
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    yew::start_app::<App>();
 }
